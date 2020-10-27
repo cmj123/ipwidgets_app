6ac9b341cad7d8bc066f736cd423859f3568dd22
Esuabom Dijemeni <e.dijemeni@gmail.com>
2020-10-27 11:37:30 +0000

    intslider, Box, HBox, VBox, Gridbox, Accordion, and Tabs

---
+++ ipwidgets_docs.py
@@ -0,0 +1,16 @@
+# Import key library
+import ipywidgets as widgets
+
+# Int Slider
+widgets.IntSlider(
+    value=7,
+    min=0,
+    max=10,
+    step=1,
+    description='Test:',
+    disabled=False,
+    continuous_update=False,
+    orientation='horizontal',
+    readout=True,
+    readout_format='d'
+)
